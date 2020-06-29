e6e8554e9b7c981205b64e15dc0ddc11d358a9e2
Christophe Lemercier <lemercier60@gmail.com>
2020-06-29 19:20:09 +0200

    Updates

---
+++ precache-manifest.96ac7d4d8e49ab07dfb988e55f230e81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5a77044fdf8f5db61cb90d80d195ac",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "c64299d23bcc50cac513",
+    "url": "/todo-app/static/js/2.c0dcc536.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app/static/js/2.c0dcc536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af82836015664db4cfb6",
+    "url": "/todo-app/static/js/main.96f4c9fd.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
